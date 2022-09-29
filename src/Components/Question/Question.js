9e2d7d26002e972c381a56957ecf506afa756346
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-09-29 22:29:58 +0600

    This is the

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -22,14 +22,9 @@ const Question = () => {
                 <div className="collapse-content">
                     <p tabIndex={1}>
                         React is an opensource JavaScript library that offers a visual overview of the JavaScript architecture for conventional MVC. React promises programmers a model in which substrates cannot directly influence enclosing components—data are downstream, data changes in HTML are efficiently modified, and the DOM is abstracted to boost performance using Virtual DOM.
-
                         How can we access data from the previous component if the data is just being flowed from components to components? The answer is props. React uses data transmission props that we need to transfer to various components.
-
                         The difference comes in which component the data are owned. State is owned locally and the component itself updates it. Props are owned and read-only by a parent. Props can be changed only if an upstream shift is caused by a callback function passed on to the child.
-
                         A prop can be passed on to the child in the state of a parent. They apply to the same value but can only be updated by the parent variable.
-
-
                     </p>
                 </div>
             </div>
@@ -41,22 +36,16 @@ const Question = () => {
                     <p tabIndex={2}>
                         Main reason to use useEffect in React.
                         1.useEffect use cases
-                        <li>     1. Running once on mount: fetch API data</li>
+                        <li>1. Running once on mount: fetch API data</li>
                         <li>2. Running on state change: validating input field</li>
                         <li>3.Running on state change: live filtering</li>
                         <li>4. Running on state change: trigger animation    on      new array value</li>
                         <li>5.Running on props change: update paragraph list on fetched API data update</li>
                         <li>6. Running on props change: updating fetched API data to get BTC updated price</li>
-
                     </p>
                 </div>
             </div>
-
         </div>
-
-
-
-
     );
 };
 
