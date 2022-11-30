e5a07a87250c5ef3b3c1d23265f860bb74aba71a
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-11-30 14:53:38 +0600

    Thisi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,38 @@
+import { faSunPlantWilt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer p-10 bg-base-200 text-base-content">
+                <div>
+                    <FontAwesomeIcon className='text-3xl' icon={faSunPlantWilt} />
+                    <p>Health Is Wealth<br />Providing reliable tech since 1992</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
